1ea2ae7f758b7324838b8667ba0e1b277255ae19
Rozhkov Sergey <progger007@gmail.com>
2017-05-23 21:57:04 +0300

    Добавлены файлы поведения

---
+++ Assets/Scripts/Behaviours/ConstructorOnly/CreateItemBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateItemBehaviour : MonoBehaviour {
+
+	private ArrayList listeners;
+
+	private GameObject clone;
+
+	void OnMouseDown()
+	{
+		clone = (GameObject)Instantiate(gameObject, gameObject.transform.position, Quaternion.identity);
+
+		clone.AddComponent<MoveBehaviour>();
+		Destroy(clone.GetComponent<CreateItemBehaviour>());
+	}
+
+	void OnMouseUp()
+	{
+		//Destroy(clone.GetComponent<CreateItemBehaviour>());
+	}
+
+	public void addListener(Object obj)
+	{
+		
+	}
+}
---
+++ Assets/Scripts/Behaviours/MoveBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBehaviour : MonoBehaviour {
+
+	private ArrayList listeners;
+
+	Vector3 dist;
+	float posX;
+	float posY;
+	Vector3 previousPosition = new Vector3(0,0,0);
+
+	private bool dragLocked = false;
+
+	void OnMouseDown()
+	{
+		previousPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+		dist = Camera.main.WorldToScreenPoint(transform.position);
+		posX = Input.mousePosition.x - dist.x;
+		posY = Input.mousePosition.y - dist.y;
+	}
+	void OnMouseDrag()
+	{
+		if (!dragLocked)
+		{
+			Vector3 curPos = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y - posY, dist.z);
+			Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
+			transform.position = worldPos;
+		}
+	}
+	void OnMouseUp()
+	{
+		previousPosition = transform.position;
+		dragLocked = false;
+
+	}
+	public void SetPreviousPosition()
+	{
+		dragLocked = true;
+		Debug.Log(previousPosition.x.ToString() + previousPosition.y.ToString());
+		gameObject.transform.position = new Vector3(previousPosition.x, previousPosition.y, previousPosition.z);
+	}
+
+	public void addListener(MoveListener obj)
+	{ 
+		
+	}
+}
