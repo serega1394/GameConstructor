c8fb4868838886290f550dc2b016a401b5abfcd5
Rozhkov Sergey <progger007@gmail.com>
2017-05-23 22:26:02 +0300

    Добавлны оставшиеся файлы

---
+++ Assets/Scripts/ButtonMark.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonMark : MonoBehaviour {
+
+	public GameObject globalGameObject;
+
+	public int mark = 0;
+
+	public void SetLevel()
+	{
+		globalGameObject.GetComponent<LevelManager>().clearCurrentLevel();
+		globalGameObject.GetComponent<LevelManager>().setCurrentLevel(mark);
+	}
+
+}
---
+++ Assets/Scripts/ScrollContentButtonMark.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScrollContentButtonMark : MonoBehaviour {
+
+	public ConstructorInterfaceManager constructorIntarfaceManager;
+
+	public string folder = "";
+
+	public Item item;
+
+	public void SetGame()
+	{
+		constructorIntarfaceManager.SetGame(this);
+	}
+	public void SetLevel()
+	{
+		constructorIntarfaceManager.SetLevel(this);
+	}
+	public void SetImgFolder()
+	{ 
+		constructorIntarfaceManager.SetImgFolder(this);
+	}
+
+	public void CreateSprite()
+	{
+		constructorIntarfaceManager.CreateSprite(gameObject.GetComponent<Image>().sprite, (Item)item.Clone());
+	}
+
+}
